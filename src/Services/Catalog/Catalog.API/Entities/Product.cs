d83ea2ce6ebf262271d5750eb45dcfb5968eb80e
Eduart <EHajko@optimaalbania.com>
2022-01-16 14:40:44 +0100

    initial

---
+++ src/Services/Catalog/Catalog.API/Entities/Product.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Entities
+{
+    public class Product
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string Id { get; set; }
+        [BsonElement("Name")]
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Summary { get; set; }
+        public string Description { get; set; }
+        public string ImageFile { get; set; }
+        public decimal Price { get; set; }
+    }
+}
