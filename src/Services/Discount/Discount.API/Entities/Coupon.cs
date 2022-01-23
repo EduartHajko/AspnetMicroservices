d39da29e1e6f27f38798776daf63e772990e0bc6
Eduart <EHajko@optimaalbania.com>
2022-01-23 18:16:07 +0100

    finished discount repository

---
+++ src/Services/Discount/Discount.API/Entities/Coupon.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Discount.API.Entities
+{
+    public class Coupon
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+        public int Amount { get; set; }
+    }
+}
